3257cf3718810d59825688d6135148a9707d3b9b
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2025-04-24 08:23:50 +0300

    trial5cors

--- Backend./app.py
+++ Backend./app.py
@@ -113,8 +113,6 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(
-        host=os.getenv('FLASK_RUN_HOST', '0.0.0.0'),
-        port=int(os.getenv('FLASK_RUN_PORT', 5000)),
-        debug=os.getenv('FLASK_DEBUG', 'False') == 'True'
-    )
+    port = int(os.environ.get("PORT", 5000))  # required by Render
+    app.run(host="0.0.0.0", port=port)
+
